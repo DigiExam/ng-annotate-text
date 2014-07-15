87f5114da11598e53f331684541724a0c8c01a1b
Robin Andersson <me@robinwassen.com>
2014-07-15 15:17:40 +0200

    Recommit of 2.4.0 build files

--- dist/2.4.0/js/ng-annotate-2.4.0.js
+++ dist/2.4.0/js/ng-annotate-2.4.0.js
@@ -36,7 +36,7 @@
       if (annotation.children.length) {
         text = parseAnnotations(text, annotation.children, annotation.startIndex + indexOffset);
       }
-      text = insertAt(text, annotation.startIndex + indexOffset, "<span class=\"ng-annotation ng-annotation-" + annotation.id + " " + annotation.type + "\" data-annotation-id=\"" + annotation.id + "\">");
+      text = insertAt(text, annotation.startIndex + indexOffset, "<span class=\"ng-annotation ng-annotation-" + annotation.id + " ng-annotation-type-" + annotation.type + "\" data-annotation-id=\"" + annotation.id + "\">");
     }
     return text;
   };
