7ca5c3174d4b2387f79c52487ce1663a3f5b2e9d
Kenneth Sundqvist <eviloatmeal@gmail.com>
2014-07-18 15:17:28 +0200

    Migrate from Grunt to Gulp.

--- README.md
+++ README.md
@@ -5,12 +5,14 @@ Visit [digiexam.github.io/ng-annotate](http://digiexam.github.io/ng-annotate)
 ## Getting started with development
 
 1. Install NodeJS ([nodejs.org](http://nodejs.org/))
-2. Install Grunt globally: `npm install -g grunt-cli`
+2. Install Gulp globally: `npm install -g gulp`
 3. Fork the repo and clone it. ([How to do it with GitHub.](https://help.github.com/articles/fork-a-repo))
 4. Go into the project folder: `cd ng-annotate`
 5. Check out the gh-pages branch: `git checkout gh-pages`
 6. Install the project dependencies: `npm install`
-7. Build the project files: `grunt serve` 
+7. Build the project files and launch a web server: `gulp serve` 
+
+To make development of ng-annotate easier you can check out the `master` branch in one directory and the `gh-pages` branch in another, then symlink the unstable files from `master` into the `lib` directory in `gh-pages`, and change the includes in `index.html` to use the unstable versions.
 
 ## Browser compatibility
 
---
+++ gulpfile.js
@@ -0,0 +1,102 @@
+"use strict";
+
+var gulp = require("gulp");
+var r = require("gulp-load-plugins")();
+
+var extensionRegexp = /\.(css|js)$/;
+var suffixRegexp = new RegExp("\\.(min|css$|js$)", "g");
+var sourceMappingURLRegexp = /\n?\/(\/|\*)#\s*sourceMappingURL=.+(\*\/)?\w*$/;
+
+var setSuffix = function(newSuffix) {
+	return function(path) {
+		// Maps add the extension to the basename so rewrite it at the end after adding the suffix.
+		var ext = path.basename.match(extensionRegexp);
+		path.basename = path.basename.replace(suffixRegexp, "") + newSuffix + (ext ? ext[0] : '');
+	}
+};
+
+var src = {
+	js: "src/*.coffee",
+	css: "src/*.scss",
+	lib: [
+		"bower_components/angular/angular.js",
+		"bower_components/jquery/dist/jquery.js",
+		"bower_components/ng-annotate/dist/*latest.min*"
+	]
+};
+
+var dest = {
+	dist: "dist",
+	lib: "lib"
+};
+
+gulp.task("default", function() {
+	return gulp.start("js", "css", "lib");
+});
+
+gulp.task("watch", function() {
+	gulp.watch(src.js, ["js"]);
+	gulp.watch(src.css, ["css"]);
+	gulp.watch(src.lib, ["lib"]);
+	return gulp.start("default");
+});
+
+gulp.task("serve", function() {
+	gulp.src('./')
+			.pipe(r.webserver({
+				port: 3000,
+				open: "http://localhost:3000/"
+			}));
+	return gulp.start("watch");
+});
+
+gulp.task("js", function() {
+	var stream = gulp.src(src.js)
+			.pipe(r.sourcemaps.init())
+			.pipe(r.coffee());
+
+	// Full
+	stream
+			.pipe(r.sourcemaps.write("./"))
+			.pipe(gulp.dest(dest.dist));
+
+	// Minified
+	stream
+			// Mangle will shorten variable names which breaks the AngularJS dependency injection.
+			// TODO: Use a build tool to preserve the important variables instead of disabling mangle.
+			.pipe(r.uglify({mangle: false}))
+			.pipe(r.rename(setSuffix(".min")))
+			.pipe(r.replace(sourceMappingURLRegexp, ''))
+			.pipe(r.sourcemaps.write("./"))
+			.pipe(gulp.dest(dest.dist));
+
+	return stream;
+});
+
+gulp.task("css", function() {
+	var stream = gulp.src(src.css)
+			.pipe(r.rubySass())
+			.pipe(r.sourcemaps.init({loadMaps: true}))
+			.pipe(r.autoprefixer("last 2 versions", "ie >= 9", "Firefox ESR"));
+
+	// Full
+	stream
+			.pipe(r.sourcemaps.write('./'))
+			.pipe(gulp.dest(dest.dist));
+
+	// Minified
+	stream
+			// TODO: minifyCss breaks source maps
+			.pipe(r.minifyCss())
+			.pipe(r.rename(setSuffix(".min")))
+			.pipe(r.replace(sourceMappingURLRegexp, ''))
+			.pipe(r.sourcemaps.write("./"))
+			.pipe(gulp.dest(dest.dist));
+
+	return stream;
+});
+
+gulp.task("lib", function() {
+	return gulp.src(src.lib)
+			.pipe(gulp.dest(dest.lib));
+});
