803ef2e39cee024abbc336d78dba068ee67babac
Kenneth Sundqvist <eviloatmeal@gmail.com>
2014-07-21 15:38:03 +0200

    Only dist minified versions and their maps of the demo CSS and JS.

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,6 @@
 var gulp = require("gulp");
 var r = require("gulp-load-plugins")();
 
-var extensionRegexp = /\.(css|js)$/;
-var suffixRegexp = new RegExp("\\.(min|css$|js$)", "g");
-var sourceMappingURLRegexp = /\n?\/(\/|\*)#\s*sourceMappingURL=.+(\*\/)?\w*$/;
-
-var setSuffix = function(newSuffix) {
-	return function(path) {
-		// Maps add the extension to the basename so rewrite it at the end after adding the suffix.
-		var ext = path.basename.match(extensionRegexp);
-		path.basename = path.basename.replace(suffixRegexp, "") + newSuffix + (ext ? ext[0] : '');
-	}
-};
-
 var src = {
 	js: "src/*.coffee",
 	css: "src/*.scss",
@@ -63,49 +51,27 @@ gulp.task("livereload", function() {
 });
 
 gulp.task("js", function() {
-	var stream = gulp.src(src.js)
+	return gulp.src(src.js)
 			.pipe(r.sourcemaps.init())
-			.pipe(r.coffee());
-
-	// Full
-	stream
-			.pipe(r.sourcemaps.write("./"))
-			.pipe(gulp.dest(dest.dist));
-
-	// Minified
-	stream
+			.pipe(r.coffee())
 			// Mangle will shorten variable names which breaks the AngularJS dependency injection.
 			// TODO: Use a build tool to preserve the important variables instead of disabling mangle.
 			.pipe(r.uglify({mangle: false}))
-			.pipe(r.rename(setSuffix(".min")))
-			.pipe(r.replace(sourceMappingURLRegexp, ''))
+			.pipe(r.rename({suffix: ".min"}))
 			.pipe(r.sourcemaps.write("./"))
 			.pipe(gulp.dest(dest.dist));
-
-	return stream;
 });
 
 gulp.task("css", function() {
-	var stream = gulp.src(src.css)
+	return gulp.src(src.css)
 			.pipe(r.rubySass())
 			.pipe(r.sourcemaps.init({loadMaps: true}))
-			.pipe(r.autoprefixer("last 2 versions", "ie >= 9", "Firefox ESR"));
-
-	// Full
-	stream
-			.pipe(r.sourcemaps.write('./'))
-			.pipe(gulp.dest(dest.dist));
-
-	// Minified
-	stream
+			.pipe(r.autoprefixer("last 2 versions", "ie >= 9", "Firefox ESR"))
 			// TODO: minifyCss breaks source maps
 			.pipe(r.minifyCss())
-			.pipe(r.rename(setSuffix(".min")))
-			.pipe(r.replace(sourceMappingURLRegexp, ''))
+			.pipe(r.rename({suffix: ".min"}))
 			.pipe(r.sourcemaps.write("./"))
 			.pipe(gulp.dest(dest.dist));
-
-	return stream;
 });
 
 gulp.task("lib", function() {
